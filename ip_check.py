63429aa86d946c13bd868bde2983bdc51b741e22
Aineah-Simiyu <76047184+Aineah-Simiyu@users.noreply.github.com>
2021-01-07 11:08:39 +0300

    Update ip_check.py

--- ip_check.py
+++ ip_check.py
@@ -3,6 +3,11 @@ import asyncio
 import json
 import sys
 
+#To Ommit the Event Closed because of using aiohttp 
+if sys.version_info[0] == 3 and sys.version_info[1] >=8 and sys.platform.startswith('win'):
+    asyncio.set_event_loop_policy(async.WindowsSelectorEventLoopPolicy())
+
+
 url = 'https://api.myip.com'
 
 async def check_ip():
