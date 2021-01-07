a1183d7439a996af8190408cadc4b2a810f9bda6
Aineah-Simiyu <76047184+Aineah-Simiyu@users.noreply.github.com>
2021-01-07 11:07:05 +0300

    Update ip_check.py

---
+++ ip_check.py
@@ -0,0 +1,13 @@
+import aiohttp
+import asyncio
+import json
+
+url = 'https://api.myip.com'
+
+async def check_ip():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            r = await resp.text()
+            results = json.loads(r)
+            print(results)
+asyncio.run(check_ip)                       
