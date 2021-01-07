afbd7d3918e002d0a391c90cfe634cfd36f66da6
Aineah-Simiyu <76047184+Aineah-Simiyu@users.noreply.github.com>
2021-01-07 11:19:00 +0300

    Update ip_check.py

--- ip_check.py
+++ ip_check.py
@@ -2,10 +2,14 @@ import aiohttp
 import asyncio
 import json
 import sys
+import colorama
+from colorama import Fore
+colorama.init()
 
 #To Ommit the Event Closed because of using aiohttp 
+
 if sys.version_info[0] == 3 and sys.version_info[1] >=8 and sys.platform.startswith('win'):
-    asyncio.set_event_loop_policy(async.WindowsSelectorEventLoopPolicy())
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 url = 'https://api.myip.com'
@@ -15,6 +19,10 @@ async def check_ip():
         async with session.get(url) as resp:
             r = await resp.text()
             results = json.loads(r)
-            print(results)
+            ip = results['ip']
+            country = results['country']
+            country_code = results['cc']
+            print(Fore.GREEN+'[+]'+Fore.RESET+ ' Your Public Ip: '+Fore.LIGHTCYAN_EX+f'{ip}\n'+Fore.RESET+Fore.GREEN+
+                  '[+]'+Fore.RESET+ ' Your Country:'+f'{country}\n'+Fore.GREEN+'[+]'+Fore.RESET+ ' Your Country Code:'+f'{country_code}\n')
             
 asyncio.run(check_ip())                       
